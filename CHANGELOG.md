75872b0e1885518dd16caaacc4ed8e351ee3f831
Ethan Huang <blesserx@gmail.com>
2018-08-23 16:37:31 +0800

    Add tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
 
+- Add tests
+
 ## [v0.1.1] - 2018-08-23
 - Remove SPM support
 - Single target
--- Tests/CupertinoJWTTests.swift
+++ Tests/CupertinoJWTTests.swift
@@ -7,8 +7,19 @@
 //
 
 import XCTest
+@testable import CupertinoJWT
 
 class CupertinoJWTTests: XCTestCase {
+    let keyID = "GG8TWG834Q"
+    let teamID = "SE9N6X5V7C"
+    let p8 = """
+-----BEGIN PRIVATE KEY-----
+MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgGH2MylyZjjRdauTk
+xxXW6p8VSHqIeVRRKSJPg1xn6+KgCgYIKoZIzj0DAQehRANCAAS/mNzQ7aBbIBr3
+DiHiJGIDEzi6+q3mmyhH6ZWQWFdFei2qgdyM1V6qtRPVq+yHBNSBebbR4noE/IYO
+hMdWYrKn
+-----END PRIVATE KEY-----
+"""
     
     override func setUp() {
         super.setUp()
@@ -19,17 +30,15 @@ class CupertinoJWTTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+
+    func testJWT() {
+        let jwt = JWT(keyID: keyID, teamID: teamID, issueDate: Date(), expireDuration: 60 * 60)
+
+        do {
+            let token = try jwt.sign(with: p8)
+            print("Generated JWT: \(token)")
+        } catch {
+            XCTFail(error.localizedDescription)
         }
     }
-    
 }

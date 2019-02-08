53890ab36d22f9e4694d6c048708b3aace8a8565
mwcs01 <jim@midwestcompsol.net>
2019-02-08 15:05:55 -0600

    Update JWT.swift
    Added JWTAppConnectAPI to work with AppConnect API.

--- Sources/JWT.swift
+++ Sources/JWT.swift
@@ -73,3 +73,74 @@ public struct JWT: Codable {
         return token
     }
 }
+
+public struct JWTAppConnectAPI: Codable {
+    private struct Header: Codable {
+        /// alg
+        let algorithm: String = "ES256"
+
+        /// kid
+        let keyID: String
+
+        enum CodingKeys: String, CodingKey {
+            case algorithm = "alg"
+            case keyID = "kid"
+        }
+    }
+
+    private struct Payload: Codable {
+        /// iss
+        public let issID: String
+
+        /// aud
+        public let aud: String
+        
+        /// iat
+        public let issueDate: Int
+
+        /// exp
+        public let expireDate: Int
+
+        enum CodingKeys: String, CodingKey {
+            case issID = "iss"
+            case aud = "aud"
+            case issueDate = "iat"
+            case expireDate = "exp"
+        }
+    }
+
+    private let header: Header
+
+    private let payload: Payload
+
+    public init(keyID: String, issID: String, aud: String, issueDate: Date, expireDuration: TimeInterval) {
+
+        header = Header(keyID: keyID)
+
+        let iat = Int(issueDate.timeIntervalSince1970.rounded())
+        let exp = iat + Int(expireDuration)
+
+        payload = Payload(issID: issID, aud: aud, issueDate: iat, expireDate: exp)
+    }
+
+    /// Combine header and payload as digest for signing.
+    public func digest() throws -> String {
+        let headerString = try JSONEncoder().encode(header.self).base64EncodedURLString()
+        let payloadString = try JSONEncoder().encode(payload.self).base64EncodedURLString()
+        return "\(headerString).\(payloadString)"
+    }
+
+    /// Sign digest with P8(PEM) string. Use the result in your request authorization header.
+    public func sign(with p8: P8) throws -> String {
+        let digest = try self.digest()
+
+        let signature = try p8.toASN1()
+            .toECKeyData()
+            .toPrivateKey()
+            .es256Sign(digest: digest)
+
+        let token = "\(digest).\(signature)"
+        return token
+    }
+}
+
